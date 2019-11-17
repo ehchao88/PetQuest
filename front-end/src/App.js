6e7a1c5745ee4de6d70db1d7f7f32c50e25dd1ba
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-11-16 20:16:56 -0500

    design changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import Main from './components/main';
 import './App.css';
 
+// firebase stuff
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './back-end/firebaseConfig';
+
 function App() {
   return (
     <div className="App">
-        <header className="App-header">
-            <Main />
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        </header>
+        <Main className={"App-header"}/>
     </div>
   );
 }
