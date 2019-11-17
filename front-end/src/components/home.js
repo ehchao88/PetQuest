cd4a7eab2df1b2f895b08ebc682d4b31e4ddb606
Nat Hsu <natalie.d.hsu@gmail.com>
2019-11-17 05:06:34 -0500

    Animated logo at home.

--- front-end/src/components/home.js
+++ front-end/src/components/home.js
@@ -3,13 +3,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 // npm install materialize-css@next
 import { Link } from 'react-router-dom'
+import "./style/home.css"
 
 class Home extends React.Component {
     render() {
         return <>
             <div className="container text-sm-center pt-10">
-                <h1 className="display-2">PetQuest</h1>
-                <p className="lead">The virtual pet to-do list</p>
+                <img id="header" src={ require("../assets/logo.gif")}/>
+                <p>The virtual pet to-do list</p>
                 <div className="btn-group mt-12" role="group" aria-label="Callout Buttons">
                     <Link to="/login" className="btn btn-primary">Login</Link>
                 </div>
--- front-end/src/components/main.js
+++ front-end/src/components/main.js
@@ -4,14 +4,14 @@ import Login from './login';
 import Dashboard from './dashboard';
 import {AuthProvider} from "./Auth";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
+// import PrivateRoute from './components/PrivateRoute';
 
 const Main = () => (
     <Router>
         <Switch>
             <Route exact path='/' component={Home}></Route>
             <Route exact path='/login' component={Login}></Route>
-            <PrivateRoute component={Dashboard} path="/dashboard" exact />
+            {/*<PrivateRoute component={Dashboard} path="/dashboard" exact />*/}
         </Switch>
     </Router>
 );
