9f079eb5f7f9789b99b12d914159f24cb5254d9f
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-11-17 01:05:27 -0500

    more components

---
+++ front-end/src/components/Auth.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import config from "./firebase.js";
+
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        // config.auth().onAuthStateChanged(setCurrentUser);
+    }, []);
+
+    return (
+        <AuthContext.Provider
+            value={{
+                currentUser
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
--- front-end/src/components/main.js
+++ front-end/src/components/main.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Home from './home';
 import Login from './login';
+import { AuthProvider } from "./Auth";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const Main = () => (
-    <Router>
-        <Switch>
-            <Route exact path='/' component={Home}></Route>
-            <Route exact path='/login' component={Login}></Route>
-        </Switch>
-    </Router>
+    <AuthProvider>
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Home}></Route>
+                <Route exact path='/login' component={Login}></Route>
+            </Switch>
+        </Router>
+    </AuthProvider>
 );
 
 export default Main;
